c283cded9e1cae01de33559df234875878257c28
Connor Mackay <connor.mackay@gmail.com>
2022-08-16 10:35:27 -0400

    implement feed/link queries

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+// Hardcoded Sample data
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
 const typeDefs = gql`
   type Query {
-    "a Simple type for getting started!"
-    hello: String!
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    type: ID!
+    description: String!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "world",
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
