42057f4dc71b3861a15e77522f912ee54874fef6
Connor Mackay <connor.mackay@gmail.com>
2022-08-19 13:58:01 -0400

    add Post route

--- src/script.js
+++ src/script.js
@@ -3,6 +3,13 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
+  const newLink = await prisma.link.create({
+    data : {
+      description: 'its google, you know what it is',
+      url: 'www.google.ca'
+    }
+  })
+  
   const allLinks = await prisma.link.findMany()
   console.log(allLinks);
 };
